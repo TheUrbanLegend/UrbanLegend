c58bf2d519c8e6109d6d9d164691e65c75b279b1
Frank Wei <frank@frankwei.xyz>
2018-10-22 10:01:15 +0800

    contract call is now ok, so is the slot sell hpy function

--- src/blockchain/index.js
+++ src/blockchain/index.js
@@ -1,21 +1,9 @@
-import { currentGetters, eos, rpc } from './store'
-
 /*
 ⚠️ Important WARNING
 a major breaking rewrite for eosjs. Be sure to check out the new doc. ⚠️
 https://eosio.github.io/eosjs/
 */
 
-// export function getBalancesByContract ({ tokenContract = 'eosio.token', accountName, symbol }) {
-//   return eos().getCurrencyBalance(tokenContract, accountName, symbol)
-// }
-export function getBalancesByContract ({ tokenContract = 'eosio.token', accountName, symbol }) {
-  return rpc().get_currency_balance(tokenContract, accountName, symbol)
-}
-
-export function getMyBalancesByContract ({ tokenContract = 'eosio.token', symbol }) {
-  const accountName = currentGetters().account.name
-  return getBalancesByContract({ tokenContract, accountName, symbol })
-}
-
 export { transferToken, transferTokenViaEosjs } from './transfer'
+export { singleContractCall, transact } from './eos'
+export { getBalancesByContract, getMyBalancesByContract } from './api'
